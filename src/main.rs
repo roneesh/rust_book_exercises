0a3ec92be3ed6de1ed222a73b10e4b11a7e169ff
Roneesh <roneesh@gmail.com>
2016-07-13 15:13:24 -0400

    4.3 done

--- src/main.rs
+++ src/main.rs
@@ -100,4 +100,55 @@ fn main() {
     let f: fn(i32) -> i32 = plus_one;
     let f = plus_one;
     let six = f(5);
+
+    //Conclusions:
+    //1. functions are easy, 
+    //2. functions take types in parens()
+    //3. functions return types with -> :type:
+    //3. variables can point to functions
+
+// 4.3 Primitive Types
+    let x = true;
+    let y : bool = false;
+    let x = 'x';
+    let two_hearts = '💕';
+    println!("{}, {}, {}", x, y, two_hearts);
+
+    let x = 42; // x has type of i32 now
+    let y = 1.0; // y has type of f64 now
+
+    let a = [1,2,3]; // array of types i32, 3 elements
+    let mut m = [1,2,3]; //array of types i32, 3 elements, but it is mutable
+    let a = [0; 20]; // arry of 20 i32 elements
+    let a = [1,2,3];
+    println!("a has {} elements", a.len());
+
+    let names = ["Ron", "Carl"];
+    println!("name 1 is: {}", names[1]);
+    
+    let a = [0,1,2,3]; //i32 arary of 4 len()
+    let complete = &a[..];
+    let middle = &a[1..4];
+    println!("array slices complete, {:?} and middle {:?}", complete, middle);
+    
+    let tup = (1, "hello");
+    let tup2 : (i32, &str) = (1, "hello");
+
+    let (x,y,z) = (1, "hello", 3);
+    println!("z is: {}", z);
+    
+    let tuppy = (1, 2, 3);
+
+    let x = tuppy.0;
+    let y = tuppy.1;
+    let z = tuppy.2;
+
+    println!("x is {}", x);
+
+    //Conclusions:
+    //1. char types are a single quote
+    //2. isize and usize are signed and unsigned types that are appropriate for the architecture
+    //3. A slice is a view into a piece of data, right now it seems you only slice arrays, but I'm sure that changes later on
+    //4. Rust's str type is more primitive than it's char type? I guess so.
+    //5 Read &str as a string slice
 }
